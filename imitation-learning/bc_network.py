6412aba8665d051c2a7c913c42be02f842fccf70
dokyun-kim4 <donny5557@gmail.com>
2024-11-20 14:10:18 -0500

    finish mvp network for behavior cloning

---
+++ imitation-learning/bc_network.py
@@ -0,0 +1,41 @@
+"""
+Behavior Cloning Neural Network
+"""
+import torch
+import torch.nn as nn
+from collections import OrderedDict
+from torchsummary import summary
+
+# TODO: change architecture to match Pusher observationSpace and actionSpace
+class BCnetwork(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.network = nn.Sequential(OrderedDict([
+            ('fc1', nn.Linear(input_dim, 256)),
+            ('relu1', nn.ReLU()),
+            ('fc2', nn.Linear(256, 128)),
+            ('relu2', nn.ReLU()),
+            ('fc3',nn.Linear(128, output_dim)),
+            ('tanh',nn.Tanh()) # Squish between [-1, 1]
+        ]))
+
+        self.scalar = 2.0 
+
+    def forward(self, x):
+        x = self.network(x)
+        x = x * self.scalar # Action space values need to be [-2, 2]
+        return x
+    
+
+if __name__ == "__main__":
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = BCnetwork(input_dim = 23, output_dim=7).to(device)
+
+    summary(model, (1, 23))
+
+    # Check that model behaves as expected
+    input = torch.linspace(-10, 10, 23).to(device)
+    output = model.forward(input)
+
+    print(f"Input size {input.shape} -> Output size {output.shape}")
