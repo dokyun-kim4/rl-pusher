9b37a14250d878d3e1a8160785d075c8fd731a37
dokyun-kim4 <donny5557@gmail.com>
2024-11-21 15:08:06 -0500

    full train+test loop implemented

--- imitation-learning/train_bc.py
+++ imitation-learning/train_bc.py
@@ -9,29 +9,30 @@ from bc_network import BCnetwork
 from loader import load_dataset
 
 # Load in expert dataset
-dataloader, env = load_dataset("pusher/expert-v0", batch_size=10)
+dataloader, env = load_dataset("pusher/expert-v1", batch_size=100)
 obs_space, act_space = env.observation_space, env.action_space
 assert isinstance(obs_space, spaces.Box)
 assert isinstance(act_space, spaces.Box)
 input_dim, output_dim = np.prod(obs_space.shape), np.prod(act_space.shape)
 
 
-# Initialize model, loss function, optimizer
-model = BCnetwork(input_dim=input_dim, output_dim=output_dim)
-loss_fn = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters())
-
 
 NUM_EPOCHS = 50
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model.to(device)
+# Initialize model, loss function, optimizer
+model = BCnetwork(input_dim=input_dim, output_dim=output_dim).to(device)
+loss_fn = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters())
+
+print(device)
+
 
 for epoch in tqdm(range(NUM_EPOCHS)):
     for batch in dataloader:
-        cur_state = batch["observations"][:, :-1] # We dont need last observation, remove it
+        cur_state = batch["observations"][:, :-1].to(device) # We dont need last observation, remove it
         agent_action = model(cur_state.to(torch.float32))
-        expert_action = batch["actions"]
+        expert_action = batch["actions"].to(device)
 
         batch_loss = loss_fn(agent_action, expert_action)
 
@@ -42,17 +43,19 @@ for epoch in tqdm(range(NUM_EPOCHS)):
     print(f"Current Epoch {epoch}/{NUM_EPOCHS}; Loss: {batch_loss}")
 
 
-# n_episodes = 10
-# env = gym.make('Pusher-v5', render_mode="human")
-# for i in tqdm(range(n_episodes)):
-#     obs, _ = env.reset()
-#     while True:
-#         action = model(torch.Tensor(obs))
-#         obs, rew, terminated, truncated, info = env.step(action)
+n_episodes = 10
+env = gym.make('Pusher-v5', render_mode="human")
+for i in tqdm(range(n_episodes)):
+    obs, _ = env.reset()
+    while True:
+        # action = env.action_space.sample()
+        
+        with torch.no_grad():
+            action = model(torch.Tensor(obs).to(device))
+            action = action.cpu().numpy()
+            obs, rew, terminated, truncated, info = env.step(action)
+        env.render()
 
-#         env.render()
-
-#         if terminated or truncated:
-#             break
-
-# env.close()
+        if terminated or truncated:
+            break
+env.close()
