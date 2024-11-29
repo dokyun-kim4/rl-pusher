fe0ea11dbb89de4b80c2982fb17383411c5ef7d2
dokyun-kim4 <donny5557@gmail.com>
2024-11-28 22:04:16 -0500

    refactor training loop into function

--- imitation-learning/train_bc.py
+++ imitation-learning/train_bc.py
@@ -8,55 +8,67 @@ from tqdm import tqdm
 from bc_network import BCnetwork
 from loader import load_dataset
 
-# Load in expert dataset
-dataloader, env = load_dataset("pusher/expert-v1", batch_size=100)
-obs_space, act_space = env.observation_space, env.action_space
-assert isinstance(obs_space, spaces.Box)
-assert isinstance(act_space, spaces.Box)
-input_dim, output_dim = np.prod(obs_space.shape), np.prod(act_space.shape)
-
-
-
-NUM_EPOCHS = 50
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Initialize model, loss function, optimizer
-model = BCnetwork(input_dim=input_dim, output_dim=output_dim).to(device)
-loss_fn = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters())
+def train_bc(model_name: str, data_path: str, batch_size: int, num_epochs: int):
 
-print(device)
+    model_path = f"models/{model_name}.pth"
 
-
-for epoch in tqdm(range(NUM_EPOCHS)):
-    for batch in dataloader:
-        cur_state = batch["observations"][:, :-1].to(device) # We dont need last observation, remove it
-        agent_action = model(cur_state.to(torch.float32))
-        expert_action = batch["actions"].to(device)
-
-        batch_loss = loss_fn(agent_action, expert_action)
-
-        optimizer.zero_grad()
-        batch_loss.backward()
-        optimizer.step()
-
-    print(f"Current Epoch {epoch}/{NUM_EPOCHS}; Loss: {batch_loss}")
+    # Load in expert dataset
+    dataloader, env = load_dataset(path = data_path, batch_size = batch_size)
+    obs_space, act_space = env.observation_space, env.action_space
+    assert isinstance(obs_space, spaces.Box)
+    assert isinstance(act_space, spaces.Box)
+    input_dim, output_dim = np.prod(obs_space.shape), np.prod(act_space.shape)
 
 
-n_episodes = 10
-env = gym.make('Pusher-v5', render_mode="human")
-for i in tqdm(range(n_episodes)):
-    obs, _ = env.reset()
-    while True:
-        action = env.action_space.sample()
-        
-        with torch.no_grad():
-            action = model(torch.Tensor(obs).to(device))
-            action = action.cpu().numpy()
+    # Initialize model, loss function, optimizer
+    model = BCnetwork(input_dim=input_dim, output_dim=output_dim).to(device)
+    loss_fn = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters())
 
-        obs, rew, terminated, truncated, info = env.step(action)
-        env.render()
+    print(f"Training on {'GPU' if device == 'cuda' else 'CPU'}")
 
-        if terminated or truncated:
-            break
-env.close()
+    for epoch in tqdm(range(num_epochs)):
+        for batch in dataloader:
+            cur_state = batch["observations"][:, :-1].to(device) # We dont need last observation, remove it
+            agent_action = model(cur_state.to(torch.float32))
+            expert_action = batch["actions"].to(device)
+
+            batch_loss = loss_fn(agent_action, expert_action)
+
+            optimizer.zero_grad()
+            batch_loss.backward()
+            optimizer.step()
+
+        print(f"Current Epoch {epoch}/{num_epochs}; Loss: {batch_loss}")
+
+    torch.save(model.state_dict(), model_path)
+    return model_path
+
+
+
+if __name__ == "__main__":
+
+    model_pth = train_bc(model_name="test_bc", data_path="pusher/expert-v0", batch_size=100, num_epochs=10)
+    model = BCnetwork(23,7)
+    model.load_state_dict(torch.load(model_pth, weights_only=True))
+    model.eval()
+
+    n_episodes = 10
+    env = gym.make('Pusher-v5', render_mode="human")
+    for i in tqdm(range(n_episodes)):
+        obs, _ = env.reset()
+        while True:
+            action = env.action_space.sample()
+            
+            with torch.no_grad():
+                action = model(torch.Tensor(obs).to(device))
+                action = action.cpu().numpy()
+
+            obs, rew, terminated, truncated, info = env.step(action)
+            env.render()
+
+            if terminated or truncated:
+                break
+    env.close()
