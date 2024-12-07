b2b3cf238cadfb34c969b8fa1abd7446c3f3df52
dfriishecht <dfriishecht@olin.edu>
2024-12-07 16:26:16 -0500

    Add generate_batches()

--- reinforcement-learning/ppo_model.py
+++ reinforcement-learning/ppo_model.py
@@ -15,3 +15,18 @@ class PPOMemory:
         self.dones = []
 
         self.batch_size = batch_size
+
+    def generate_batches(self):
+        n_states = len(self.states)
+        batch_start = np.arange(0, n_states, self.batch_size)
+        idx = np.arange(n_states, dtype=np.int64)
+        np.random.shuffle(idx) # Add randomness for Random SGD
+        batches = [idx[i:i+self.batch_size] for i in batch_start]
+
+        return np.array(self.states),\
+                np.array(self.actions),\
+                np.array(self.probs),\
+                np.array(self.vals),\
+                np.array(self.rewards),\
+                np.array(self.dones),\
+                batches
