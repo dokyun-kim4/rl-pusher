aaadc6601e1bc1cec404ac0732d819a0536c9d52
dfriishecht <dfriishecht@olin.edu>
2024-12-07 17:01:49 -0500

    Add checkpoint loading/saving

--- reinforcement-learning/ppo_model.py
+++ reinforcement-learning/ppo_model.py
@@ -116,6 +116,11 @@ class ActorNetwork(nn.Module):
 
         return dist
     
+    def save_checkpoint(self):
+        torch.save(self.state_dict(), self.checkpoint_file)
+    
+    def load_checkpoint(self):
+        self.load_state_dict(torch.load(self.checkpoint_file))
 
 
 
